6c6d98c82a6f052287b8b4619e3264a8b8db8e75
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2024-09-05 16:45:27 +0700

    Bài 35.2.3 : Đổi mật khẩu

--- routes/client/user.route.js
+++ routes/client/user.route.js
@@ -7,4 +7,5 @@ router.post("/register", controller.register);
 router.post("/login", controller.login);
 router.post("/password/forgot", controller.forgotPassword);
 router.post("/password/otp", controller.otpPassword);
+router.patch("/password/reset", controller.resetPassword);
 module.exports = router;
